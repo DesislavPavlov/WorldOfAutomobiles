c1967d4d2313c865c8edd4d5c7f8f63ec36ec308
DesislavPavlov <124437772+DesislavPavlov@users.noreply.github.com>
2025-01-31 10:28:46 +0200

    Complete index logic and style

--- index.js
+++ index.js
@@ -3,33 +3,62 @@ const parallaxContainer = document.querySelector('.video-container');
 const content = document.querySelector('#history-section');
 const scrollIndicator = document.querySelector('.scroll-indicator');
 
+// Listen for scrolling
+// Desktop
+let readyForHiding = true;
+hideInfo();
+document.addEventListener('wheel', handleWheel);
 
-console.log(video);
-console.log(parallaxContainer);
-console.log(content);
-setInterval(() => {
-    console.log(window.scrollY);
-    
-}, 500);
-
-
-function handleScroll(e) {
-  const nigga = e.deltaY;
-
-    if (nigga > 0) {
-        scrollIndicator.style.display = 'none';
-        content.style.display = 'grid'; 
-        video.style.transform = `scale(1.20)`;
-        video.style.filter = `blur(7px) brightness(0.1)`;
+function handleWheel(e) {
+    const deltaY = e.deltaY;
+  
+    if (deltaY > 0) {
+        revealInfo();
     } 
-    else if (nigga < 0 && window.scrollY == 0) {
+    else if (deltaY < 0 && content.scrollTop == 0) {
+        hideInfo();
+    }
+}
+
+// Touchscreen
+let startY = 0;
+document.addEventListener("touchstart", (e) => {
+    startY = e.touches[0].clientY;
+});
+
+document.addEventListener("touchmove", (e) => {
+    let deltaY = e.touches[0].clientY;
+    
+    if (deltaY < startY) {
+        revealInfo();
+    } else if (deltaY > startY && content.scrollTop == 0) {
+        hideInfo();
+    }
+});
+
+scrollIndicator.addEventListener("touchstart", () => {
+    revealInfo();
+});
+
+function revealInfo() {
+    readyForHiding = false;
+
+    scrollIndicator.style.display = 'none';
+    content.style.display = 'grid'; 
+    video.style.transform = `scale(1.20)`;
+    video.style.filter = `blur(7px) brightness(0.1)`;
+}
+
+function hideInfo() {
+    if (readyForHiding) {
         scrollIndicator.style.display = 'block';
         content.style.display = 'none';
         video.style.transform = `scale(1)`;
         video.style.filter = `blur(0px) brightness(1)`;
     }
+    else {
+        setTimeout(() => {
+            readyForHiding = true;
+        }, 300);
+    }
 }
-
-// Listen for the 'wheel' event (scrolling)
-window.addEventListener('wheel', handleScroll);
-window.addEventListener('touchstart', console.log("akkkakakaka"));
